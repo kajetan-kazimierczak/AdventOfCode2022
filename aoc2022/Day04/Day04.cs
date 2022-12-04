6142bab4032cec26b867d709803f0b984b87b93b
Kajetan Kazimierczak <kajetan@hotmail.com>
2022-12-04 12:55:52 +0100

    Day 4

---
+++ aoc2022/Day04/Day04.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc2022.Day04
+{
+    internal class Day04
+    {
+        public void Run()
+        {
+            var data = File.ReadAllLines(@"Day04\input.txt");
+            //var data = "2-4,6-8\r\n2-3,4-5\r\n5-7,7-9\r\n2-8,3-7\r\n6-6,4-6\r\n2-6,4-8".Split("\r\n");
+
+            var ans1 = Part1(data);
+            var ans2 = Part2(data);
+        }
+
+        public int Part1(string[] data)
+        {
+            var result = 0;
+
+            foreach (var line in data)
+            {
+                var elfs = line.Split(",");
+                var elf1 = elfs[0].Split("-");
+                var elf2 = elfs[1].Split("-");
+
+                if (int.Parse(elf1[0]) <= int.Parse(elf2[0]) && int.Parse(elf1[1]) >= int.Parse(elf2[1])
+                    || int.Parse(elf2[0]) <= int.Parse(elf1[0]) && int.Parse(elf2[1]) >= int.Parse(elf1[1]))
+                {
+                    result++;
+                }
+
+            }
+            return result;
+        }
+
+        public int Part2(string[] data)
+        {
+            var result = 0;
+
+            foreach (var line in data)
+            {
+                var elfs = line.Split(",");
+                var elf1 = elfs[0].Split("-");
+                var elf2 = elfs[1].Split("-");
+
+
+                if (int.Parse(elf1[1]) >= int.Parse(elf2[0]) && int.Parse(elf1[0]) <= int.Parse(elf2[1]))
+                {
+                    result++;
+                }
+
+            }
+            return result;
+        }
+    }
+}
--- aoc2022/Program.cs
+++ aoc2022/Program.cs
@@ -3,9 +3,12 @@
 using aoc2022.Day01;
 using aoc2022.Day02;
 using aoc2022.Day03;
+using aoc2022.Day04;
 
 Console.WriteLine("Hello, World!");
 
 //new Day01().Run();
 //new Day02().Run();
 //new Day03().Run();
+//new Day04().Run();
+
