f9db436c0b955ad483c7a133723912ad08209313
Kajetan Kazimierczak <kajetan@hotmail.com>
2022-12-06 08:39:06 +0100

    Day 6

---
+++ aoc2022/Day06/Day06.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc2022.Day06
+{
+    internal class Day06
+    {
+        public void Run()
+        {
+            var data = File.ReadAllLines(@"Day06\input.txt")[0];
+
+            var ans1 = FindStart(data, 4);
+            var ans2 = FindStart(data, 14);
+        }
+
+        public int FindStart(string input, int length)
+        {
+            for (int i = 0; i < input.Length - length; i++)
+            {
+                var a = input.Substring(i, length);
+
+                if (a.Distinct().Count() == length)
+                {
+                    return i + length;
+                }
+            }
+            return 0;
+        }
+    }
+}
--- aoc2022/Program.cs
+++ aoc2022/Program.cs
@@ -1,11 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using System.Collections.Immutable;
+using System.Runtime.InteropServices.Marshalling;
+
 Console.WriteLine("Hello, World!");
 
 //new aoc2022.Day01.Day01().Run();
 //new aoc2022.Day02.Day02().Run();
 //new aoc2022.Day03.Day03().Run();
 //new aoc2022.Day04.Day04().Run();
-new aoc2022.Day05.Day05().Run();
+//new aoc2022.Day05.Day05().Run();
+new aoc2022.Day06.Day06().Run();
+
+
 
